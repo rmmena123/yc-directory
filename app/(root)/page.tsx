76afb82f77eb7275b4ac812b32eb7cc791c4c489
Rodrigo Mena <rodrigo.mena8632@gmail.com>
2025-08-06 09:39:35 -0300

    Feat: Author authentication flow

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
@@ -11,6 +12,9 @@ export default async function Home({
   const query = (await searchParams).query;
   const params = { search: query || null };
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
+  const session = await auth();
+  console.log(session);
 
   return (
     <>
