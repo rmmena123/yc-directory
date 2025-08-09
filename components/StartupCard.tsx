abed05271ed36fcb25f2b2eb710cb0dc8805c7ea
Rodrigo Mena <rodrigo.mena8632@gmail.com>
2025-08-08 22:17:39 -0300

    Feat: User page + responsive layout

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,10 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupCardType = Omit<Startup, "author"> & { author?: Author };
 
@@ -43,8 +44,8 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
 
         <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/48x48"
-            alt="placeholder"
+            src={author?.image || "https://placehold.co/48x48"}
+            alt={author?.name || "Unknown Author"}
             width={48}
             height={48}
             className="rounded-full"
@@ -68,6 +69,18 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
         </Button>
       </div>
     </li>
+  );
+};
+
+export const StartupCardSkeleton = () => {
+  return (
+    <>
+      {[0, 1, 2, 3, 4].map((index: number) => (
+        <li key={cn("skeleton", index)}>
+          <Skeleton className="startup-card_skeleton" />
+        </li>
+      ))}
+    </>
   );
 };
 
